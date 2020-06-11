8fee825e670bd4f5e4d2f45bb85229bc562cb322
davi <davi.p.m94@gmail.com>
2020-06-10 22:34:44 -0300

    create the table

--- src/database/db.js
+++ src/database/db.js
@@ -3,3 +3,18 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./src/database/database.db");
 
 module.exports = db;
+
+db.serialize(() => {
+  db.run(`
+     CREATE TABLE IF NOT EXISTS places (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        image TEXT,
+        name TEXT,
+        address TEXT,
+        address2 TEXT,
+        state TEXT,
+        city TEXT,
+        items TEXT
+     );
+  `);
+});
